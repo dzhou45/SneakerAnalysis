4a13def5965fea105155386eeec2a4e0a38a980e
Kad <qiang.z@husky.neu.edu>
2020-03-23 00:10:37 -0400

    3/23

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var db = require('./db');
+var cors = require('cors');
+var helmet = require('helmet');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require('./routes/api');
+
+var app = express();
+
+// mysql db
+// app.use('/comments', require('./controllers/comments'));
+// app.use('/users', require('./controllers/users'));
+db.connect(db.MODE_PRODUCTION, function (err) {
+  if (err) {
+    console.log('Unable to connect to MySQL.');
+    process.exit(1);
+  } else {
+    app.listen(3000, function () {
+      console.log('Listening on port 3000...');
+    });
+  }
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(helmet());
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', testAPIRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.sendStatus(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+});
+
+
+router.get('/welcome', function (req, res, next) {
+  res.render('welcome', {
+    username: req.cookies.username
+  });
+});
+
+router.get('/story/:storyId', function (req, res, next) {
+  res.send(`<h1>${req.params.storyId}</h1>`)
+});
+
+
+router.post('/process_login', function (req, res, next) {
+
+  const password = req.body.password;
+  const username = req.body.username;
+
+  if (password === "a") {
+    res.cookie('username', username);
+    res.redirect('/welcome')
+  } else {
+    res.redirect('/?msg=fail')
+  }
+});
+
+router.get('/statement', function (req, res, next) {
+  res.download(path.join(__dirname, 'userStatements/statement.png'))
+});
+
+router.get('/logout', function (req, res, next) {
+  res.clearCookie('username');
+  res.redirect('/')
+});
+
+module.exports = router;
