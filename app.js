3b8d5eac2b8fae9ec4e18d3987831ad1f9478aef
Kad <qiang.z@husky.neu.edu>
2020-03-27 18:41:49 -0400

    movie app

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 var db = require('./db');
 var cors = require('cors');
 var helmet = require('helmet');
+// Discord Passport
+const passport = require('passport');
+const DiscordStrategy = require('passport-discord').Strategy;
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var testAPIRouter = require('./routes/api');
@@ -15,12 +19,12 @@ var app = express();
 // mysql db
 // app.use('/comments', require('./controllers/comments'));
 // app.use('/users', require('./controllers/users'));
-db.connect(db.MODE_PRODUCTION, function (err) {
+db.connect(db.MODE_PRODUCTION, function(err) {
   if (err) {
     console.log('Unable to connect to MySQL.');
     process.exit(1);
   } else {
-    app.listen(3000, function () {
+    app.listen(3000, function() {
       console.log('Listening on port 3000...');
     });
   }
@@ -31,32 +35,63 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(helmet());
+
+// discord-passport config
+app.use(
+  session({
+    secret: 'express',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+var discordStrat = new DiscordStrategy(
+  {
+    clientID: '691896556261212211',
+    clientSecret: '5LDqjvgsq_Nk2LWi4j3BBRCvXnk3DeGd',
+    callbackURL: 'http://localhost:9000/auth',
+    scope: ['identify', 'email', 'guilds'],
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    return cb(null, profile);
+  }
+);
+passport.use(discordStrat);
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', indexRouter);
 app.use('/api', testAPIRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+  next(createError(404, '404 Not Found'));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.sendStatus(err.status || 500);
+  res.status(err.status || 500);
   res.render('error');
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,80 @@
 var express = require('express');
 var path = require('path');
+var request = require('request');
 var router = express.Router();
+const passport = require('passport');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index');
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
 });
 
-router.get('/login', function (req, res, next) {
-  res.render('login');
+router.get('/login', passport.authenticate('discord'));
+router.get(
+  '/auth',
+  passport.authenticate('discord', {
+    successRedirect: '/',
+    failureRedirect: '/loginFailed',
+  })
+);
+
+router.use((req, res, next) => {
+  res.locals.imageBaseUrl = imageBaseUrl;
+  next();
 });
 
-
-router.get('/welcome', function (req, res, next) {
-  res.render('welcome', {
-    username: req.cookies.username
+router.get('/', function(req, res, next) {
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    const parseData = JSON.parse(movieData);
+    res.render('index', {
+      parseData: parseData.results,
+    });
   });
 });
 
-router.get('/story/:storyId', function (req, res, next) {
-  res.send(`<h1>${req.params.storyId}</h1>`)
+router.use(isLoggedIn);
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/login');
+}
+
+router.get('/movie/:id', function(req, res, next) {
+  const movieId = req.params.id;
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+
+  request.get(thisMovieUrl, (error, response, movieData) => {
+    const parseData = JSON.parse(movieData);
+    res.render('single-movie', {
+      parseData,
+    });
+  });
 });
 
-
-router.post('/process_login', function (req, res, next) {
-
-  const password = req.body.password;
-  const username = req.body.username;
-
-  if (password === "a") {
-    res.cookie('username', username);
-    res.redirect('/welcome')
-  } else {
-    res.redirect('/login?msg=fail')
-  }
+router.get('/favorites', function(req, res, next) {
+  console.log(req.user.username);
+  res.json({ username: req.user.username });
 });
 
-router.get('/statement', function (req, res, next) {
-  res.download(path.join(__dirname, 'userStatements/statement.png'))
-});
+router.post('/search', function(req, res, next) {
+  const userSearchTerm = encodeURI(req.body.movieSearch);
+  const cat = req.body.cat;
+  const movieUrl = `${apiBaseUrl}/search/${cat}/?query=${userSearchTerm}&api_key=${apiKey}`;
 
-router.get('/logout', function (req, res, next) {
-  res.clearCookie('username');
-  res.redirect('/login')
+  request.get(movieUrl, (error, response, movieData) => {
+    let parseData = JSON.parse(movieData);
+
+    if (cat === 'person') {
+      parseData.results = parseData.results[0].known_for;
+    }
+
+    res.render('index', { parseData: parseData.results });
+  });
 });
 
 module.exports = router;
